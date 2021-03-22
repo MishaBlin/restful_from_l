1430f0b4d43cf871608bce6796a3831f3a290390
MOVS Laboratory <lab.movs@outlook.com>
2021-03-22 18:26:09 +0500

    heroku1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from flask import Flask, render_template, redirect, request, make_response, session, abort, jsonify
@@ -254,4 +256,6 @@ def main():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     main()
